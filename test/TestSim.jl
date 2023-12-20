fb4b001aa5d673db4b8a729a42e17a74a90ac203
Benjamin Chislett <chislett.ben@gmail.com>
2023-12-20 23:15:20 +0100

    Update sim script

--- test/TestSim.jl
+++ test/TestSim.jl
@@ -6,25 +6,16 @@ Random.seed!(1234)
 
 function main()
     NTris = 100
-    NBatch = 100
+    NBatch = 5000
     NEpoch = 5
-    NRefine = 100
+    NRefine = 250
 
     # rm("output/simresult/", force=true, recursive=true)
     mkpath("output/simresult/")
     target = float.(load("lisa.png"))
     @time res = simulate(target, NTris, NBatch, NEpoch, NRefine)
-    # save("output/simresult/final.png", res.current)
-    # Serialization.serialize("output/simresult/simres_$NTris-prims_$NBatch-batch_$NEpoch-epoch_$NRefine-refine.bin", res)
-
-    # img_acc = ones(RGB{Float32}, 200, 200)
-    # for i=1:NTris
-    #     img = ones(RGB{Float32}, 200, 200)
-    #     draw!(img, res.shapes[i], res.original_colours[i], RasterAlgorithmScanline())
-    #     draw!(img_acc, res.shapes[i], res.original_colours[i], RasterAlgorithmScanline())
-    #     save("output/simresult_single$i.png", img)
-    #     save("output/simresult_accum$i.png", img_acc)
-    # end
+    save("output/simresult/experiment.png", res.history[end].current)
+    Serialization.serialize("output/simresult/simlog_$NTris-prims_10x-blackbox_$NBatch-batch_$NEpoch-epoch_$NRefine-refine.bin", res)
 
     res
 end
