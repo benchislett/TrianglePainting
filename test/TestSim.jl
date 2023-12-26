3727d7552ed1b184a93b4b489437c665de73beed
Benjamin Chislett <chislett.ben@gmail.com>
2023-12-26 23:31:40 +0100

    Update sim script

--- test/TestSim.jl
+++ test/TestSim.jl
@@ -6,16 +6,15 @@ Random.seed!(1234)
 
 function main()
     NTris = 100
-    NBatch = 5000
-    NEpoch = 5
-    NRefine = 250
+    NBatch = 1000000
+    NEpoch = 10
+    NRefine = 100
 
     # rm("output/simresult/", force=true, recursive=true)
     mkpath("output/simresult/")
     target = float.(load("lisa.png"))
-    @time res = simulate(target, NTris, NBatch, NEpoch, NRefine)
+    @time res = simulate(target, NTris, NBatch, NEpoch, NRefine, 0.75f0)
     save("output/simresult/experiment.png", res.history[end].current)
-    Serialization.serialize("output/simresult/simlog_$NTris-prims_10x-blackbox_$NBatch-batch_$NEpoch-epoch_$NRefine-refine.bin", res)
 
     res
 end
