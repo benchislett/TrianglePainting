511535ca12ec685b12c49042bea2a4c742d33a0d
Benjamin Chislett <chislett.ben@gmail.com>
2023-12-31 12:03:31 +0100

    Reexport submodules

--- src/Paint.jl
+++ src/Paint.jl
@@ -1,44 +1,28 @@
 module Paint
+using Reexport
 
 include("Shapes.jl")
-using .Shapes2D
-export AbstractShape, AbstractPolygon
-export Point, x, y
-export AABB
-export Polygon, Triangle
-export vertices, vertex
+@reexport using .Shapes2D
 
 include("Spatial.jl")
-using .Spatial2D
-export covers
+@reexport using .Spatial2D
 
 include("Pixel.jl")
-using .Pixel
-export over
-export Loss, SELoss, AELoss, loss
+@reexport using .Pixel
 
 include("Raster.jl")
-using .Raster2D
-export u2x, v2y, x2u, y2v
-export RasterState, rasterfunc, rasterize
-export RasterAlgorithm, RasterAlgorithmScanline, RasterAlgorithmBounded, RasterAlgorithmPointwise
+@reexport using .Raster2D
 
 include("Draw.jl")
-using .Draw2D
-export draw!
-export imloss, drawloss, drawloss_batch, averagepixel, averagepixel_batch, opaquerecolor, alpharecolor
+@reexport using .Draw2D
 
 include("Mutate.jl")
-using .Mutate
-export numvars, mutate, mutate_batch
+@reexport using .Mutate
 
 include("DrawGPU.jl")
-using .GPUDraw2D
-export RasterAlgorithmGPU
+@reexport using .GPUDraw2D
 
 include("GreedySim.jl")
-using .GreedySim
-export PrimitiveSequence, SimState
-export simulate, commit!, redraw!, genbackground, simulate_iter_ga
+@reexport using .GreedySim
 
 end # module Paint
