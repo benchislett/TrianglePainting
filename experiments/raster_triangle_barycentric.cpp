b266cdc2324b078a5a6a5e342bb162cc1b4c75d5
Benjamin Chislett <chislett.ben@gmail.com>
2024-06-24 00:19:12 +0200

    Add simple experiment for manual rasterization and PNG I/O

---
+++ experiments/raster_triangle_barycentric.cpp
@@ -0,0 +1,31 @@
+#include "geometry/barycentric.h"
+
+#include "lodepng.h"
+
+#include <vector>
+
+// Draw a triangle, colour it using barycentric coordinates, and save the result to a PNG file.
+int main() {
+    const int width = 512;
+    std::vector<unsigned char> image(width * width * 4, 255);
+
+    geometry2d::triangle tri{{0.25, 0.25}, {0.75, 0.25}, {0.5, 0.75}};
+
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < width; y++) {
+            float u = (x + 0.5f) / (float)width;
+            float v = (y + 0.5f) / (float)width;
+            auto bary = geometry2d::barycentric_coordinates({u, v}, tri);
+            if (bary.u >= 0 && bary.v >= 0 && bary.w >= 0) {
+                image[(x + y * width) * 4 + 0] = (unsigned int)(bary.u * 255);
+                image[(x + y * width) * 4 + 1] = (unsigned int)(bary.v * 255);
+                image[(x + y * width) * 4 + 2] = (unsigned int)(bary.w * 255);
+                image[(x + y * width) * 4 + 3] = 255;
+            }
+        }
+    }
+
+    lodepng::encode("raster_triangle_barycentric.png", image, width, width);
+
+    return 0;
+}
