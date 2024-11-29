41dfaf60da0380cc1f6dbe896c37313f1fe31425
Benjamin Chislett <chislett.ben@gmail.com>
2024-11-29 13:41:00 -0500

    remove geometry3d (unused)

--- include/geometry/types.h
+++ include/geometry/types.h
@@ -34,18 +34,3 @@ namespace geometry {
         float radius;
     };
 };
-
-namespace geometry3d {
-    struct point {
-        float x, y, z;
-    };
-    
-    struct triangle {
-        point a, b, c;
-    };
-
-    struct sphere {
-        point center;
-        float radius;
-    };
-};
