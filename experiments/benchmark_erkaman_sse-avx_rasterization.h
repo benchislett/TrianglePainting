47fa635e09662d56d497d632f5baa05e173224a7
Benjamin Chislett <chislett.ben@gmail.com>
2025-03-26 23:15:52 -0400

    start moving sse/avx into utils

--- experiments/benchmark_erkaman_sse-avx_rasterization.h
+++ experiments/benchmark_erkaman_sse-avx_rasterization.h
@@ -7,46 +7,11 @@
 #include <iostream>
 #include <cassert>
 
-// get SSE and AVX
 #include <immintrin.h>
 #include <xmmintrin.h>
 
 #include "rasterize.h"
 #include "utils.h"
-
-struct vec2 {
-public:
-	float x;
-	float y;
-
-	vec2() : x(0.0f), y(0.0f) { }
-	vec2(const float x_, const float y_) : x(x_), y(y_) { }
-};
-
-unsigned int rounddownAligned(unsigned int i, unsigned int align) {
-	return (unsigned int)floor((float)i / (float)align) * align;
-}
-
-unsigned int roundupAligned(unsigned int i, unsigned int align) {
-	return (unsigned int)ceil((float)i / (float)align) * align;
-}
-
-__m128 edgeFunctionSSE(const vec2 &a, const vec2 &b, __m128 cx, __m128 cy)
-{
-	return _mm_sub_ps(_mm_mul_ps(_mm_sub_ps(cx, _mm_set1_ps(a.x)), _mm_set1_ps(b.y - a.y)), _mm_mul_ps(_mm_sub_ps(cy, _mm_set1_ps(a.y)), _mm_set1_ps(b.x - a.x)));
-}
-
-__m256 edgeFunctionAVX(const vec2 &a, const vec2 &b, __m256 cx, __m256 cy)
-{
-	return _mm256_sub_ps(_mm256_mul_ps(_mm256_sub_ps(cx, _mm256_set1_ps(a.x)), _mm256_set1_ps(b.y - a.y)), _mm256_mul_ps(_mm256_sub_ps(cy, _mm256_set1_ps(a.y)), _mm256_set1_ps(b.x - a.x)));
-}
-
-float edgeFunction(const vec2 &a, const vec2 &b, const vec2 &c)
-{
-	// we are doing the reversed edge test, compared to the article.
-	// we need to do it in this way, since our coordinate system has the origin in the top-left corner.
-	return (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x);
-}
 
 void alphaBlendInPlace(unsigned char* buf, unsigned char r, unsigned char g, unsigned char b, unsigned char a) {
 	buf[0] = (r * (255 - a) + (buf[0] * a)) / 255;
--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <immintrin.h>
+#include <xmmintrin.h>
+
 int clampi(int i, int min, int max) {
 	if (i < min) {
 		return min;
@@ -30,4 +33,38 @@ float min(float x, float y) {
 
 float max(float x, float y) {
 	return x > y ? x : y;
+}
+
+struct vec2 {
+public:
+	float x;
+	float y;
+
+	vec2() : x(0.0f), y(0.0f) { }
+	vec2(const float x_, const float y_) : x(x_), y(y_) { }
+};
+
+unsigned int rounddownAligned(unsigned int i, unsigned int align) {
+	return (unsigned int)floor((float)i / (float)align) * align;
+}
+
+unsigned int roundupAligned(unsigned int i, unsigned int align) {
+	return (unsigned int)ceil((float)i / (float)align) * align;
+}
+
+__m128 edgeFunctionSSE(const vec2 &a, const vec2 &b, __m128 cx, __m128 cy)
+{
+	return _mm_sub_ps(_mm_mul_ps(_mm_sub_ps(cx, _mm_set1_ps(a.x)), _mm_set1_ps(b.y - a.y)), _mm_mul_ps(_mm_sub_ps(cy, _mm_set1_ps(a.y)), _mm_set1_ps(b.x - a.x)));
+}
+
+__m256 edgeFunctionAVX(const vec2 &a, const vec2 &b, __m256 cx, __m256 cy)
+{
+	return _mm256_sub_ps(_mm256_mul_ps(_mm256_sub_ps(cx, _mm256_set1_ps(a.x)), _mm256_set1_ps(b.y - a.y)), _mm256_mul_ps(_mm256_sub_ps(cy, _mm256_set1_ps(a.y)), _mm256_set1_ps(b.x - a.x)));
+}
+
+float edgeFunction(const vec2 &a, const vec2 &b, const vec2 &c)
+{
+	// we are doing the reversed edge test, compared to the article.
+	// we need to do it in this way, since our coordinate system has the origin in the top-left corner.
+	return (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x);
 }
